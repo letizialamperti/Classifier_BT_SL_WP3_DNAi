6f70c88c3e62d717d6283e069478169f55a6ce24
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2025-02-28 16:53:01 +0100

    Update training_BarlowTwins.py

--- training_BarlowTwins.py
+++ training_BarlowTwins.py
@@ -36,7 +36,7 @@ checkpoint_dir.mkdir(parents=True, exist_ok=True)
 checkpoint_callback = ModelCheckpoint(
     monitor='val_barlow_loss',
     dirpath=checkpoint_dir,
-    filename='BT_sud_cose_1_dataset-{epoch:02d}',
+    filename='BT_460_dataset-{epoch:02d}',
     save_top_k=3,
     mode='min',
 )

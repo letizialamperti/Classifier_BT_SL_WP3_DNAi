5d5decfe66868d4c7ade08d50fe08d38e460ea1b
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2025-11-30 11:15:41 +0100

    Update run_binary_dann_460.sh

--- run_binary_dann_460.sh
+++ run_binary_dann_460.sh
@@ -47,7 +47,7 @@ for fold in {1..5}; do
     --initial_learning_rate 1e-3 \
     --max_epochs 100 \
     --accelerator gpu \
-    --lambda_domain 1.0 \
+    --lambda_domain 0 \
     --seed 42
 
   # train_binary_dann.py should save e.g.:

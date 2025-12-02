525f79ed097e48ce34c5f76746d0a13e288d7240
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2025-12-02 11:15:42 +0100

    Update tsne_dann_run.sh

--- tsne_dann_run.sh
+++ tsne_dann_run.sh
@@ -25,7 +25,7 @@ PROTECTION="label/labels_5_levels.csv"
 HABITAT="habitat/label_habitat_460.csv"
 
 # === Choose the checkpoint you want to visualize ===
-CHECKPOINT="checkpoints_dann_classifier/lambda_4_0/split_5_fold_03-best.ckpt"
+CHECKPOINT="checkpoints_dann_classifier/lambda_2_0/split_5_fold_04-best.ckpt"
 
 # ==========================
 #      t-SNE ARGUMENTS
@@ -33,7 +33,7 @@ CHECKPOINT="checkpoints_dann_classifier/lambda_4_0/split_5_fold_03-best.ckpt"
 NUM_CLASSES=5
 BATCH_SIZE=64
 PERPLEXITY=20
-LAMBDA_DOMAIN=4
+LAMBDA_DOMAIN=2
 
 echo "Running t-SNE visualization for checkpoint: $CHECKPOINT"
 

8bb207e711716a59ca61d19f825f6ed2d4852421
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2025-12-04 12:05:57 +0100

    Update train_binary_dann_cv.py

--- train_binary_dann_cv.py
+++ train_binary_dann_cv.py
@@ -140,7 +140,7 @@ def main():
         # Early stopping: monitoriamo la stessa metrica
         early_stopping_callback = EarlyStopping(
             monitor='val_task_loss',
-            patience=3,
+            patience=10,
             mode='min'
         )
 

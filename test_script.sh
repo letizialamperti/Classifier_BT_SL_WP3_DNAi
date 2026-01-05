2e959cf439caf145713e9c26e30fced681d3dd1b
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2026-01-05 15:08:38 +0100

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -25,7 +25,7 @@ PROTECTION_FILE="label/labels_4_levels.csv"
 HABITAT_FILE="habitat/empty_label_habitat_460.csv"
 
 # === Checkpoint da testare ===
-CHECKPOINT="CV_Class/d65rwe4c/checkpoints/epoch=13-step=140.ckpt"
+CHECKPOINT="CV_Class/tngp3v87/checkpoints/epoch=31-step=320.ckpt"
 NUM_CLASSES=4    # solo per compatibilità con lo script Python
 BATCH_SIZE=32
 

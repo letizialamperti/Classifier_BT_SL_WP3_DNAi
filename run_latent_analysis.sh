595f605af4a42096e483093e09f09175973bae70
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2026-01-12 10:09:16 +0100

    Update run_latent_analysis.sh

--- run_latent_analysis.sh
+++ run_latent_analysis.sh
@@ -25,10 +25,10 @@ PROTECTION="label/labels_4_levels.csv"
 HABITAT="habitat/empty_label_habitat_460.csv"
 
 HABITAT_COL="habitat/label_habitat_460.csv"
-K_CROSS_FILE="k_cross/new_split_4_fold_03.csv"
+K_CROSS_FILE="k_cross/new_split_4_fold_04.csv"
 
 # === Checkpoint da analizzare ===
-CHECKPOINT="CV_Class/4ipmwyet/checkpoints/epoch=53-step=594.ckpt"
+CHECKPOINT="CV_Class/ix2dj0bn/checkpoints/epoch=81-step=820.ckpt"
 
 # ==========================
 #      ANALYSIS PARAMETERS
